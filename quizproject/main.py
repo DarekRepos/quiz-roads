d83661a98b9700a9c80691ab869e9955cf776282
Darek <dareczek06@gmail.com>
2023-01-24 18:57:03 +0100

    added base routes file

---
+++ quizproject/main.py
@@ -0,0 +1,25 @@
+from flask import Blueprint, render_template
+from flask_login import login_required, current_user
+from . import db
+
+#    export FLASK_APP=project
+#    export FLASK_DEBUG=1
+
+
+
+main = Blueprint('main',__name__)
+
+@main.route("/")
+def index():
+    return render_template('index.html')
+
+
+@main.route("/profile")
+@login_required
+def profile():
+    return  render_template('profile.html', name=current_user.user_name)
+
+@main.route("/quiz")
+@login_required
+def quiz():
+    return  render_template('quiz.html')
