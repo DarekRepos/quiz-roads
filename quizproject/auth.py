7bc5486e6a1db6e33d371f72e92900572aa9240d
Darek <dareczek06@gmail.com>
2023-01-25 17:01:38 +0100

    added validation to the lkogin form

--- quizproject/auth.py
+++ quizproject/auth.py
@@ -5,33 +5,36 @@ from .models import User
 from flask_login import login_user, login_required, logout_user
 from . import db
 
+from .forms.loginform import LoginForm
+
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+# @auth.route('/login', methods=['GET'])
+# def login():
+#     form = LoginForm(request.form)
+#     return render_template('login.html', form=form)
+
+
+@auth.route('/login', methods=["GET", "POST"])
 def login():
-    return render_template('login.html')
-
-
-@auth.route('/login', methods=['POST'])
-def login_post():
  
-    email = request.form.get('email')
-    password = request.form.get('password')
-    remember = True if request.form.get('remember') else False
-
-    user = User.query.filter_by(user_email=email).first()
-
-    # check if the user actually exists
-    # take the user-supplied password, hash it, and compare it to the hashed password in the database
-    if not user or not check_password_hash(user.user_password, password):
-        flash('Please check your login details and try again.')
+    form = LoginForm(request.form)
     
-    # if the above check passes, then we know the user has the right credentials
-    login_user(user, remember=remember)
-    #TODO: update last login field
+    if form.validate_on_submit():
+        user = User.query.filter_by(user_email=form.email.data).first()
 
-    return redirect(url_for('main.profile'))
+        # check if the user actually exists
+        # take the user-supplied password, hash it, and compare it to the hashed password in the database
+        if not user or not check_password_hash(user.user_password, password):
+            flash('Please check your login details and try again.')
+        
+        # if the above check passes, then we know the user has the right credentials
+        login_user(user, remember=remember)
+
+        return redirect(url_for('main.profile'))
+        
+    return render_template('login.html', form=form)
 
 @auth.route('/signup')
 def signup():
