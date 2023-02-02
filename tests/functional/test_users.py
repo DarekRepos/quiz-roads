d2f1d822aff6cdefb5458c43ff643e0baa1998d9
Darek <dareczek06@gmail.com>
2023-02-02 13:35:21 +0100

    added tests added basic functional test  #2

---
+++ tests/functional/test_users.py
@@ -0,0 +1,22 @@
+
+
+def test_home_page(client):
+    """
+    GIVEN a Flask application configured for testing
+    WHEN the '/' page is requested (GET)
+    THEN check that the response is valid
+    """
+
+    response = client.get('/')
+    assert response.status_code == 200
+
+
+def test_home_page_post(client):
+    """
+    GIVEN a Flask application configured for testing
+    WHEN the '/' page is requested (POST)
+    THEN check that the status code a '405' is returned
+    """
+
+    response = client.post('/')
+    assert response.status_code == 405
