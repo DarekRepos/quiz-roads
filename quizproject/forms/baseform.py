be709389cbb3160c8c9e4189c6bd1ba74f2318ac
Dareczek <dareczek06@gmail.com>
2023-02-06 20:36:41 +0100

    Configure crsf protection
    - enable crsf protection globally
    - added key to .env for crsf
    - disable (comment code) session crsf for forms
    - added crsf error page (status code 400 )

--- quizproject/forms/baseform.py
+++ quizproject/forms/baseform.py
@@ -1,20 +1,20 @@
 
-from datetime import timedelta
-from flask_wtf import FlaskForm
-from flask import session, Flask
-from wtforms.csrf.session import SessionCSRF
-
-app = Flask(__name__)
-# TODO: import secrete from env file
+# from datetime import timedelta
+# from flask_wtf import FlaskForm
+# from flask import session, Flask
+# from wtforms.csrf.session import SessionCSRF
 
 
-class MyBaseForm(FlaskForm):
-    class Meta:
-        csrf = True
-        csrf_class = SessionCSRF
-        csrf_secret = b'EPj00jpfj8Gx1SjnyLxwBBSQfnQ9DJYe0Ym'
-        csrf_time_limit = timedelta(minutes=20)
 
-    @property
-    def csrf_context(self):
-        return session
+
+# class MyBaseForm(FlaskForm):
+#     class Meta:
+#         csrf = True
+#         csrf_class = SessionCSRF
+#         #csrf_secret = app.config['CSRF_SECRET_KEY']
+#         csrf_secret = b'EPj00jpfj8Gx1SjnyLxwBBSQfnQ9DJYe0Ym'
+#         csrf_time_limit = timedelta(minutes=20)
+
+#     @property
+#     def csrf_context(self):
+#         return session
