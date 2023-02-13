9b8a15e124b1994612447e1ccd91de830cfe445b
Dareczek <dareczek06@gmail.com>
2023-02-13 23:08:56 +0100

    added tests added simple functional register test

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,15 +1,22 @@
-from flask import Flask
+import datetime
 import pytest
+from sqlalchemy import delete
 
 from config import TestingConfig
 
 # import the module
-from quizproject import create_app, db
+from quizproject import create_app
+from quizproject import db as _db
+from quizproject.models import User
+
+from werkzeug.security import generate_password_hash
 
 
-@pytest.fixture()
+@pytest.fixture(scope="session")
 def app():
-    app = Flask(__name__)
+    """
+    Returns session-wide application.
+    """
 
     app = create_app(TestingConfig)
 
@@ -18,16 +25,44 @@ def app():
         "SECRET_KEY": 'test',
     })
 
-    with app.app_context():
-        db.create_all()
+    ctx = app.test_request_context()
+    ctx.push()
+    yield app
+    ctx.pop()
+
+
+@pytest.fixture(scope="session")
+async def app_with_db(app):
+    _db.create_all()
 
     yield app
 
-    # clean up / reset resources here
+    _db.session.commit()
+    _db.drop_all()
 
 
 @pytest.fixture()
-def client(app):
+def app_with_user(app_with_db):
+    register_time = datetime.datetime.utcnow()
+    new_user = User(register_time=register_time,
+                    user_email="dareczek014@gmail.com",
+                    user_password=generate_password_hash(
+                                  'dareczek014',
+                                  method='sha256'),
+                    user_name='dareczek014')
+
+    # add the new user to the database
+    _db.session.add(new_user)
+    _db.session.commit()
+
+    yield app_with_db
+    _db.session.execute(delete(User))
+    _db.session.commit()
+    _db.session.remove()
+
+
+@pytest.fixture()
+def client(app, app_with_db):
     return app.test_client()
 
 
@@ -41,7 +76,7 @@ class AuthActions(object):
     def __init__(self, client):
         self._client = client
 
-    def login(self, email='dareczek011@gmail.com', password='dareczek011'):
+    def login(self, email, password):
         return self._client.post(
             '/login',
             data={'email': email, 'password': password},
--- tests/functional/test_auth.py
+++ tests/functional/test_auth.py
@@ -1,29 +1,26 @@
 import pytest
-from flask import g, session, url_for
-from quizproject import engine
+from flask import session
+from sqlalchemy import select
+
+from quizproject.models import User
 
 
-def test_login(client, auth):
+def test_login(client, auth, app_with_db, app_with_user):
     """
-    GIVEN a Flask application configured for testing
+    GIVEN a Flask application configured for testing 
+          (test user are created with app_with_user)
     WHEN the '/login' page is requested (POST) with VALID data
-    THEN check that thge user can enter login page (page loaded - 200 status code)
-         check that existing user is successfully login
+    THEN check that the user can enter login page (page loaded - 200 status code)
+         check that existing user is successfully login (status 200)
          check that user is successfully redirected to profile page
     """
 
     assert client.get('/login').status_code == 200
-    response = auth.login()
+
+    response = auth.login("dareczek014@gmail.com", "dareczek014")
+
     assert response.status_code == 200
-
     assert response.request.path == '/profile'
-
-    with client:
-        client.get('/')
-
-        # TODO: implement session
-        # assert session['user_id'] == 1
-        # assert g.user['username'] == 'dareczek011@gmail.com'
 
 
 def test_logout(client, auth):
@@ -38,16 +35,84 @@ def test_logout(client, auth):
         # get index page
         client.get('/')
 
-        auth.login()
+        # login to existing account
+        auth.login("dareczek014@gmail.com", "dareczek014")
 
+        # logout user
         response = auth.logout()
+
         # check user is successfuly loged out
         assert response.status_code == 200
 
         # Check that there was one redirect response.
         assert len(response.history) == 1
+
         # Check that the second request was to the index page.
         assert response.request.path == "/"
 
         # TODO: implement session
         # assert 'user_id' not in session
+
+
+@pytest.mark.parametrize( ('username, email, password, confirm, message'), [
+    ('dareczek046', 'dareczek046@gmail.com',
+     'dareczek046', 'dareczek046', b'login'),
+    ('dareczek014', 'newdareczek014@gmail.com',
+     'dareczek14', 'dareczek014', b'already registered'),
+    ('newdareczek014', 'dareczek014@gmail.com',
+     'dareczek14', 'dareczek014', b'already registered')
+],
+
+)
+def test_register_validation(client, app_with_db, app_with_user,
+                             username, email, password, confirm, message):
+    """
+    GIVEN a Flask application configured for testing and existing user in db
+    WHEN login with valid data
+    THEN check that existing user is successfully created (status code 200)
+         
+    """
+
+    assert client.get('/signup').status_code == 200
+    response = client.post(
+        '/signup', data={'username': username,
+                         'email': email,
+                         'password': password,
+                         'confirm': confirm},
+        follow_redirects=True
+    )
+    assert response.status_code == 200
+
+
+@pytest.mark.parametrize(('username, email, password, confirm, redirection'),
+ [
+    ('dareczek046', 'dareczek046@gmail.com',
+     'dareczek046', 'dareczek046', '/login'),
+    ('dareczek014', 'newdareczek014@gmail.com',
+     'dareczek14', 'dareczek014', '/signup'),
+    ('newdareczek014', 'dareczek014@gmail.com',
+     'dareczek14', 'dareczek014', '/signup')
+],
+)
+def test_register(client, app_with_db, app_with_user,
+                  username, email, password, confirm, redirection):
+    """
+    GIVEN a Flask application configured for testing and existing user in db
+    WHEN user register with valid data
+         user register with existing email address
+         user register with existing user name
+    THEN check that the new user is successfully created with valid data
+         check that user was redirect to page that he can change EMAIL
+         check that user was redirect to page that he can change USERNAME
+    """
+
+    assert client.get('/signup').status_code == 200
+    response = client.post(
+        '/signup', data={'username': username,
+                         'email': email,
+                         'password': password,
+                         'confirm': confirm},
+        follow_redirects=True
+    )
+    assert response.status_code == 200
+    assert response.request.path == redirection
