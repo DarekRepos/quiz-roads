5e13239e32132350159172559eb29454e85bd8ec
Dareczek <dareczek06@gmail.com>
2023-04-24 15:24:57 +0200

    Changed  model
    - added relationship to other tables

--- quizproject/models/person_answers.py
+++ quizproject/models/person_answers.py
@@ -4,21 +4,22 @@ from .. import db
 metadata = MetadataPathFinder()
 
 
-class PeresonAnswers(db.Model):
-    __tablename__ = "person_answers",
+class PersonAnswers(db.Model):
+    __tablename__ = ("person_answers",)
     metadata,
     person_answer_id = db.Column(db.Integer, primary_key=True)
-    quiz_id = db.Column(db.Integer, primary_key=True)
-    question_id = db.Column(db.Integer, primary_key=True)
-    participant_id = db.Column(db.Integer, primary_key=True)    
-    answer_id = db.Column(db.Integer, primary_key=True)    
+    quiz_id = db.Column(db.Integer, db.ForeignKey("quiz.quiz_id"))
+    question_id = db.Column(db.Integer, db.ForeignKey("question.question_id"))
+    participant_id = db.Column(db.Integer, 
+                               db.ForeignKey("participants.participant_id"))
+    answer_id = db.Column(db.Integer, db.ForeignKey("answers.answer_id"))
     question_time_start = db.Column(db.String(100), nullable=False)
     question_time_end = db.Column(db.String(100))
 
-    questions = db.relationship('Questions', backref='quiz')
+    questions = db.relationship("Questions", backref="quiz")
 
     def __repr__(self) -> str:
-        return f'<Quiz {self.person_answer_id }>'
+        return f"<Quiz {self.person_answer_idgh }>"
 
     def get_id(self):
-        return (self.person_answer_id)
+        return self.person_answer_id
