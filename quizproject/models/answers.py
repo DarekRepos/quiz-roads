cfaa0700c4ac3dcd2e601a5eebf57568c0046b61
Dareczek <dareczek06@gmail.com>
2023-04-24 15:24:44 +0200

    Changed  model
    - added relationship to other tables

--- quizproject/models/answers.py
+++ quizproject/models/answers.py
@@ -5,17 +5,19 @@ metadata = MetadataPathFinder()
 
 
 class Answers(db.Model):
-    __tablename__ = "answers",
+    __tablename__ = ("answers",)
     metadata,
     answer_id = db.Column(db.Integer, primary_key=True)
-    question_id = db.Column(db.String(100), nullable=False)
+    question_id = db.Column(
+        db.String(100), db.ForeignKey("question.question_id"), nullable=False
+    )
     question_answer = db.Column(db.String(100))
     qquestion_correct = db.Column(db.Integer)
 
-    questions = db.relationship('Questions', backref='quiz')
+    questions = db.relationship("person_answers", backref="answers")
 
     def __repr__(self) -> str:
-        return f'<Answer {self.question_answer}>'
+        return f"<Answer {self.question_answer}>"
 
     def get_id(self):
-        return (self.answer_id)
+        return self.answer_id
