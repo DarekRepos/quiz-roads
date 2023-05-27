375a7fc25e3f0615309a2d70b592cec39f33baf1
Dareczek <dareczek06@gmail.com>
2023-05-27 13:12:22 +0200

    Refactor readability

--- tests/functional/test_cli_commands.py
+++ tests/functional/test_cli_commands.py
@@ -1,41 +1,43 @@
-'''
+"""
 FUNCTIONAL TESTS - CLI FLASK
-'''
+"""
 
 
 def test_create_command(runner, app_with_db):
-
     # full command: flask questions create example
     # example - can be diffrent string
-    result = runner.invoke(args=['questions', 'create', 'example'])
-    assert "Questions collections example are created" in result.output
+    result = runner.invoke(args=["questions", "create", "example"])
+    expected = "Questions collections example are created"
+    assert expected in result.output
     assert result.exit_code == 0
 
 
 def test_questions_count_command(runner, app_with_db):
     # full command: flask question count
-    result = runner.invoke(args=['questions', 'count'])
-    assert "total 7 questions with total 28 answers" in result.output
+    result = runner.invoke(args=["questions", "count"])
+    expected = "total 7 questions with total 28 answers"
+    assert expected in result.output
     assert result.exit_code == 0
 
 
 def test_deleteall_command_when_canceled(runner, app_with_db):
     # full command: flask question deleteall
     # y - select 'y' to confirm deleting
-    result = runner.invoke(args=['questions', 'deleteall'], input="n")
+    result = runner.invoke(args=["questions", "deleteall"], input="n")
     assert result.exit_code == 1
     assert result.exception
-    assert 'Aborted!' in result.output
+    expected = "Aborted!"
+    assert expected in result.output
     # out, err = capsys.readouterr()
-    # assert out == "<class \'SystemExit\'>\n"
+    # assert out == "<class \'1SystemExit\'>\n"
     # assert err == ''
 
 
 def test_deleteall_command_when_confirmed_to_yes(runner, app_with_db, capsys):
     # full command: flask question deleteall
     # y - select 'y' to confirm deleting
-    result = runner.invoke(args=['questions', 'deleteall'], input="y")
+    result = runner.invoke(args=["questions", "deleteall"], input="y")
     assert result.exit_code == 0
     assert not result.exception
-    assert "total 7 questions and total 28 answers was deleted" in result.output
-
+    expected = "total 7 questions and total 28 answers was deleted"
+    assert expected in result.output
