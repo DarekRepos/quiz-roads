fda07804210d8cfb54e257b2409ec1cb6c7f2420
Dareczek <dareczek06@gmail.com>
2023-06-21 16:16:15 +0200

    Refactor main to blueprint

---
+++ quizproject/main/forms.py
@@ -0,0 +1,43 @@
+from flask_wtf import FlaskForm
+from wtforms import widgets, SelectMultipleField, SubmitField, RadioField
+
+# from quizproject.forms.baseform import MyBaseForm
+
+from wtforms.validators import DataRequired
+
+
+class OneValidAnswerForm(FlaskForm):
+    """ Form for one valid answer in radio fields
+
+    """
+    user_answers = RadioField(
+        "Label", coerce=int, choices=[], validators=[DataRequired()]
+    )
+
+    submit = SubmitField("Save")
+
+
+class MultipleCheckBoxField(SelectMultipleField):
+
+    """Override class for SelectedMultiple field
+
+    """
+
+    widget = widgets.ListWidget(prefix_label=False)
+    option_widget = widgets.CheckboxInput()
+
+
+class MultipleValidAnswersForm(FlaskForm):
+    """ Form for multiple valid answers in checkboxes
+
+    """
+
+    user_answers = MultipleCheckBoxField("Label", coerce=int, choices=[], validators=[])
+    submit = SubmitField("Save")
+
+
+class ResultForm(FlaskForm):
+    """ Result button that submit all answers and get score
+    """
+
+    submit = SubmitField("Complete Quiz & Check Yours Results")
